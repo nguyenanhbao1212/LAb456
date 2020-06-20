e19232f1c66313e2f5bd9422c0ae278a694ab5d0
hieu pham <phamthanhhieu20101999@gmail.com>
2020-06-20 21:42:43 +0700

    Add project files.

---
+++ Lab456/ViewModels/ValidTime.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+
+namespace Lab456.ViewModels
+{
+        public class ValidTime : ValidationAttribute
+
+        {
+            public override bool IsValid(object value)
+            {
+                DateTime dateTime;
+                var isvalid = DateTime.TryParseExact(Convert.ToString(value),
+                    "HH:mm",
+                    CultureInfo.CurrentCulture,
+                    DateTimeStyles.None,
+                    out dateTime);
+                return isvalid;
+            }
+        }
+}
